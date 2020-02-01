570d37bfdaf7edd48a197bcabf21d47ee50e2276
Peter Bray <petertheyeti@gmail.com>
2020-01-31 17:38:18 -0800

    Add manifest.json.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,24 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "FoxAndGeese",
+        short_name: "FoxAndGeese",
+        start_url: "/",
+        background_color: "#eeeeee",
+        //theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "images/logo.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        //crossOrigin: `use-credentials`,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
